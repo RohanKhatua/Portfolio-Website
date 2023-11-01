ff620dae0996840d49e7a7ddae734fea01e537ce
Rohan Khatua <rohan.khatua2021@vitstduent.ac.in>
2023-11-02 01:55:03 +0530

    feat: add dynamic app icon, add linkedin to socials

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Linkedin, Mail, Twitter } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
@@ -23,6 +23,12 @@ const socials = [
 		label: "Github",
 		handle: "RohanKhatua",
 	},
+	{
+		icon: <Linkedin size={20} />,
+		href: "https://www.linkedin.com/in/rohankhatua/",
+		label: "LinkedIn",
+		handle: "Rohan Khatua",
+	}
 ];
 
 export default function Example() {
@@ -30,13 +36,13 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+				<div className="grid grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-2 lg:gap-12">
 					{socials.map((s) => (
 						<Card>
 							<Link
 								href={s.href}
 								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+								className="p-4 relative flex flex-col items-center gap-4 duration-700 group lg:gap-6 lg:p-12"
 							>
 								<span
 									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
@@ -58,6 +64,8 @@ export default function Example() {
 					))}
 				</div>
 			</div>
+
+
 		</div>
 	);
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,46 +5,10 @@ import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
-	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
-	},
-	description: "Software engineer at upstash.com and founder of planetfall.io",
-	openGraph: {
-		title: "chronark.com",
-		description:
-			"Software engineer at upstash.com and founder of planetfall.io",
-		url: "https://chronark.com",
-		siteName: "chronark.com",
-		images: [
-			{
-				url: "https://chronark.com/og.png",
-				width: 1920,
-				height: 1080,
-			},
-		],
-		locale: "en-US",
-		type: "website",
-	},
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
-	},
-	twitter: {
-		title: "Chronark",
-		card: "summary_large_image",
-	},
-	icons: {
-		shortcut: "/favicon.png",
-	},
-};
+	title: "Rohan Khatua Portfolio",
+	description: "Rohan Khatua's Portfolio",
+}
+
 const inter = Inter({
 	subsets: ["latin"],
 	variable: "--font-inter",
@@ -64,11 +28,11 @@ export default function RootLayout({
 		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
 			<head>
 				<Analytics />
+				<link rel="icon" href="/icon?<generated>" type="image/png" sizes="32x32"/>
 			</head>
 			<body
-				className={`bg-black ${
-					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-				}`}
+				className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+					}`}
 			>
 				{children}
 			</body>
