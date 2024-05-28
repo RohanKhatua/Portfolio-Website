93728471e1afbf2ae2b9aa2e9387852c32908fd8
RohanKhatua <contact@rohankhatua.dev>
2024-05-28 19:08:33 +0530

    revalidate resume

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,27 @@ const nextConfig = {
 	experimental: {
 		mdxRs: true,
 	},
+	async headers() {
+		return [
+			{
+				source: "/resume.pdf",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+					},
+					{
+						key: "Pragma",
+						value: "no-cache",
+					},
+					{
+						key: "Expires",
+						value: "0",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default withContentlayer(nextConfig);
